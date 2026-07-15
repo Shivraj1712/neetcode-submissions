7d877433ab414f33ff22e62f40b742ec9c5c8d6d
Shivraj Maharaul <shivrajmaharaul688@gmail.com>
2026-07-15 09:06:55 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        unordered_map<char,int> freq1, freq2 ;
+        for(auto i : s) freq1[i]++;
+        for(auto i : t) freq2[i]++;
+        for(auto it : freq1){
+            if(freq2.find(it.first) == freq2.end()) return false;
+            if(freq2[it.first] != it.second) return false;
+        }
+        for(auto it : freq2){
+            if(freq2.find(it.first) == freq1.end()) return false;
+            if(freq2[it.first]  != it.second) return false;
+        }
+        return true;
+    }
+};
