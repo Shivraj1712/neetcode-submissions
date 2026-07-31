9a66bf61860884cad1f2230f1e0abdfdbf890ba1
Shivraj Maharaul <shivrajmaharaul688@gmail.com>
2026-07-31 16:32:12 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+public:
+    stack<pair<int,int>> st;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+            st.push({val,val});
+        }else if(st.top().second > val){
+            st.push({val,val});
+        }else {
+            st.push({val,st.top().second});
+        }
+    }
+    
+    void pop() {
+        if(st.empty()) return ;
+        st.pop();
+    }
+    
+    int top() {
+        if(st.empty()) return -1;
+        return st.top().first;
+    }
+    
+    int getMin() {
+        if(st.empty()) return -1;
+        return st.top().second;
+    }
+};
