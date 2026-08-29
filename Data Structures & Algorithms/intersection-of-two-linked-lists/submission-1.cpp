7ab628bbb30c6f822aa5b21631943cd4a1a8cfac
Shivraj Maharaul <shivrajmaharaul688@gmail.com>
2026-08-30 00:29:02 +0530

    Add: intersection-of-two-linked-lists - submission-1

---
+++ Data Structures & Algorithms/intersection-of-two-linked-lists/submission-1.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        unordered_map<ListNode *, int> track;
+        ListNode * temp = headA;
+        while(temp){
+            track[temp]++;
+            temp = temp ->next;
+        }
+        temp = headB;
+        while(temp){
+            if(track.count(temp)) return temp;
+            temp = temp ->next;
+        }
+        return nullptr;
+    }
+};
